ef0174f4dbd9ed725a1465b5f1a9918b0ca09b7e
Mike Conigliaro <mike@conigliaro.org>
2013-03-15 16:32:03 -0600

    Fix vbox installation failure on Ubuntu

--- definitions/.ubuntu/vbox.sh
+++ definitions/.ubuntu/vbox.sh
@@ -6,7 +6,7 @@ aptitude -y install linux-headers-$(uname -r) build-essential
 # Install VirtualBox guest additions
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
-echo "yes" | sh /mnt/VBoxLinuxAdditions.run
+echo "yes" | sh /mnt/VBoxLinuxAdditions.run || true
 umount /mnt
 rm VBoxGuestAdditions_$VBOX_VERSION.iso
 lsmod | grep -q vboxguest
