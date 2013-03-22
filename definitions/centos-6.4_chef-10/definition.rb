98a45003b7d4140c60796a9aa1d1765a1a10b68e
Mike Conigliaro <mike@conigliaro.org>
2013-03-22 16:33:59 -0600

    Use CentOS 6.4 iso

--- definitions/centos-6.4_chef-10/definition.rb
+++ definitions/centos-6.4_chef-10/definition.rb
@@ -1,9 +1,9 @@
 require File.join(File.dirname(__FILE__), '..', '.centos', 'definition.rb')
 
-iso = "CentOS-6.3-x86_64-bin-DVD1.iso"
+iso = "CentOS-6.4-x86_64-bin-DVD1.iso"
 
 Veewee::Session.declare(CENTOS_DEFINITION.merge({
   :iso_file => iso,
-  :iso_md5 => "a991defc0a602d04f064c43290df0131",
-  :iso_src => "http://mirrors.kernel.org/centos/6.3/isos/x86_64/#{iso}"
+  :iso_md5 => "0128cfc7c86072b13ee80dd013e0e5d7",
+  :iso_src => "http://mirrors.kernel.org/centos/6.4/isos/x86_64/#{iso}"
 }))
