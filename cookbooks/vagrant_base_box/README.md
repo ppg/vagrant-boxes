5865b3d2582d7ee1dddb8e98f5aa2fb7b1348e94
Mike Conigliaro <mike@conigliaro.org>
2014-07-21 16:04:41 -0600

    Move cookbooks to their own repos

--- cookbooks/vagrant_base_box/README.md
+++
@@ -1,7 +0,0 @@
-# vagrant_base_box cookbook
-
-This cookbook provisions a [Vagrant base box](http://docs.vagrantup.com/v2/boxes/base.html).
-
-## Usage:
-
-Just add `vagrant_base_box` to Packer's run list.
--- cookbooks/vagrant_base_box/metadata.rb
+++
@@ -1,7 +0,0 @@
-name 'vagrant_base_box'
-maintainer 'SendGrid'
-maintainer_email 'operations@sendgrid.com'
-license 'All rights reserved'
-description 'Installs/Configures vagrant_base_box'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
