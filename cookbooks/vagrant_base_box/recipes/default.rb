d1a6f8c975e42ec2a01f779c19dcc3f7eebc9155
Mike Conigliaro <mike@conigliaro.org>
2014-07-21 11:25:11 -0600

    Set single-request-reopen resolv.conf option

--- cookbooks/vagrant_base_box/recipes/default.rb
+++ cookbooks/vagrant_base_box/recipes/default.rb
@@ -10,6 +10,20 @@ execute 'aptitude update' do
   only_if { node['platform'] == 'ubuntu' }
 end
 
+if node['platform'] == 'centos'
+  service 'networking' do
+    action :nothing
+  end
+
+  template '/etc/sysconfig/network' do
+    variables(
+      hostname: node['fqdn']
+    )
+    mode 00644
+    notifies :restart, 'service[networking]'
+  end
+end
+
 %w(
   packages
   virtualbox
